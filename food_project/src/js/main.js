67636c3bb9a33f15a2cc28575dae7c0a5feb1a4c
AleksandrKozhevnikov97 <65402607+AleksandrKozhevnikov97@users.noreply.github.com>
2020-05-30 19:46:27 +0300

    adding__animation to tabs

--- food_project/src/js/main.js
+++ food_project/src/js/main.js
@@ -2,21 +2,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
-          tabsParent = document.querySelector('.tabheader__items')
+          tabsParent = document.querySelector('.tabheader__items');
 
     
     function hideTabContent () {
         tabsContent.forEach(item => {
-            item.style.display = 'none'
+            item.classList.add('hide');
+            item.classList.remove('show');
         });
 
         tabs.forEach(item => {
-            item.classList.remove('tabheader__item_active')
+            item.classList.remove('tabheader__item_active');
         });
     }
 
     function showTabContent (i = 0) {
-        tabsContent[i].style.display = 'block';
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+
         tabs[i].classList.add('tabheader__item_active');
     }
 
