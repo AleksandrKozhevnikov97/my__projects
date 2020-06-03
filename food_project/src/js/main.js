b15ef7b5fcfe259d929cd320f5d72f669807e486
AleksandrKozhevnikov97 <65402607+AleksandrKozhevnikov97@users.noreply.github.com>
2020-06-03 19:36:16 +0300

    modified__modal

--- food_project/src/js/main.js
+++ food_project/src/js/main.js
@@ -260,5 +260,31 @@ document.addEventListener('DOMContentLoaded', () => {
           }
         });
     });
-  }
+}
+
+function showThanksModal(message) {
+    const prevModalDialog = document.querySelector('.modal__dialog');
+
+    prevModalDialog.classList.add('hide');
+    openModal();
+
+    const thanksModal = document.createElement('div');
+    thanksModal.classList.add('modal__dialog');
+    thanksModal.innerHTML = `
+      <div class = "modal__content">
+        <div data-close class = "modal__close">×</div>
+        <div class = "modal__title">${message}</div>
+      </div>
+    `
+
+    document.querySelector('.modal').append(thanksModal);
+  setTimeout(() => {
+    thanksModal.remove();
+    prevModalDialog.classList.add('show');
+    prevModalDialog.classList.remove('hide');
+    closeModal();
+  }, 4000)
+
+}
+
 });
